22d260a3662c019f1f6dd3b39620260350053837
Arjun V H <arjunvelliyidathu@gmail.com>
2025-12-06 04:16:41 +0530

    Update setup.py metadata for PyPI release v1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="SeisPlotPy",
     version="1.0.0",
-    packages=find_packages(), 
+    packages=find_packages(),
+    py_modules=["main"],
     include_package_data=True,
     install_requires=[
         "PyQt6>=6.4.0",
@@ -22,14 +23,16 @@ setup(
     author="Arjun V H",
     author_email="arjunvelliyidathu@gmail.com",
     description="A high-performance 2D post-stack Interpretation & QC Workstation.",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/arjun-vh/SeisPlotPy",
+    keywords="seismic, segy, geophysics, visualization",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Geophysics"
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics"
     ],
     python_requires=">=3.8",
 
